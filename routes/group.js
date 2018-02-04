edd565e8780e3f41e3090f51e82363549e0d70f0
jesses <835612575@qq.com>
2018-02-04 11:23:38 +0800

    显示小组接口

--- routes/group.js
+++ routes/group.js
@@ -33,6 +33,33 @@ router.post("/creategroup", function (req, res, next) {
     })
 });
 
+router.get("/fetchgroup", function (req, res, next) {
+
+    let page = parseInt(req.param("page"));
+    let pageSize = parseInt(req.param("pageSize"));
+    let skip = (page - 1) * pageSize;
+
+    let groupModel = Group.find().populate({path:'author',select:'userName userEmail'}).skip(skip).limit(pageSize);
+    groupModel.exec(function(err,doc) {
+        if (err) {
+            res.json({
+                status:'0',
+                msg:err.message
+            });
+        } else {
+            // console.log(doc);
+            res.json({
+                status:'1',
+                msg:'',
+                result:{
+                    count:doc.length,
+                    list:doc,
+                    // author:
+                }
+            });
+        }
+    })
+})
 
 
 module.exports = router;
